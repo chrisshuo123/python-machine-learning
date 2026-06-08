6123d18161827f308202c946622d27a4f12bb228
chrisshuo123 <chr1z100@gmail.com>
2026-06-08 22:57:21 +0700

    add test and train dataset in housePrice.py file

--- kaggle-housePrice/housePrice.py
+++ kaggle-housePrice/housePrice.py
@@ -1,4 +1,9 @@
 import pandas as pd
 test = pd.read_csv(r"kaggle-housePrice\dataset\test.csv")
-print(test.head())
+""" Test: Unhash # the print below to test """
+#print(test.head())
 # print(test.head().to_string())
+
+""" Train: Unhash # the train below to train """
+train = pd.read_csv(r"kaggle-housePrice\dataset\train.csv")
+print(train.head())
